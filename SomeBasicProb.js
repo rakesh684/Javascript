162f16d86a1b44b4477e7f0a55fc13ed42c4de02
Rakesh kumar <dr.rakesh684@gmail.com>
2021-10-17 12:51:21 +0530

    Some Basic problem

---
+++ SomeBasicProb.js
@@ -0,0 +1,7 @@
+//Area od triangle taking base and height as an argument
+function AreaOfTriangle(a,b){
+  var res;
+  res=1/2*(a*b);
+  console.log(res);
+}
+AreaOfTriangle(8,4);
--- minToSec.js
+++ minToSec.js
@@ -2,3 +2,4 @@ function mTs(m){
   console.log(m*60);
 }
 mTs(30);
+
