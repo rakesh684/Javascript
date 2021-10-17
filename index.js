d0dcd3e768225b6caaa9051bb09bd12efcf17cc1
Rakesh kumar <dr.rakesh684@gmail.com>
2021-10-17 11:42:55 +0530

    Javascript practice

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-var i=1;
-while(i<=5){
-  console.log(i);
-  i++;
-}
+// var i=1;
+// while(i<=5){
+//   console.log(i);
+//   i++;
+// }
+
+// var i=1;
+// for(; i<=5 ; i++){
+//   console.log(i);
+// }
+
+//it will give infinite output
+var i=2;
+do{
+  console.log(i)
+}while(i<=5);
