239630ee0d804491f071fa3fb4f9a40ac72b368a
Rakesh kumar <dr.rakesh684@gmail.com>
2021-10-17 12:59:35 +0530

    Some Basic problem

--- SomeBasicProb.js
+++ SomeBasicProb.js
@@ -1,7 +1,23 @@
 //Area od triangle taking base and height as an argument
-function AreaOfTriangle(a,b){
-  var res;
-  res=1/2*(a*b);
-  console.log(res);
+// function AreaOfTriangle(a,b){
+//   var res;
+//   res=1/2*(a*b);
+//   console.log(res);
+// }
+// AreaOfTriangle(8,4);
+
+
+//Convert age to Days leap year is not consider
+function calAge(a){
+  if(a==0){
+    console.log("0");
+    return;
+  }
+  else if(a%4==0){
+    return;
+  }
+  else{
+    console.log(a*365);
+  }
 }
-AreaOfTriangle(8,4);
+calAge(65);
