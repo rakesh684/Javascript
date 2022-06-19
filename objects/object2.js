581f20943228525dba7804af3369977c22bd2f21
Rakesh kumar <dr.rakesh684@gmail.com>
2022-06-20 02:51:01 +0530

    added new object file

---
+++ objects/object2.js
@@ -0,0 +1,19 @@
+const bar = "Bar Boca";
+const cafe = "Aku Aku";
+const restaurant = "Colonel Mustard";
+
+const favouritePlaces = {
+    bar,
+    cafe,
+    restaurant,
+    greeting() {
+        console.log("Hello there!");
+    }
+}
+
+console.log(favouritePlaces);
+
+favouritePlaces.greeting();
+
+console.log(favouritePlaces.restaurant)
+ty
